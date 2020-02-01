19a5699d8dbc1121ace6197f092c50813525cff3
VictorGus <retaow@gmail.com>
2020-02-02 01:06:49 +0300

    New rendering model

--- ui/ls-ui/src/Map.js
+++ ui/ls-ui/src/Map.js
@@ -1,33 +1,22 @@
 import React, {Component} from 'react';
-import GoogleMapReact, {GoogleApiWrapper} from 'google-maps-react';
-
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+import {VectorMap} from 'react-jvectormap'
 
 class LSMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
-
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          google={this.props.google}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
+      <div style={{width: 1500, height: 500}}>
+      <VectorMap map={'world_mill'}
+      backgroundColor="#3b96ce"
+      ref="map"
+      containerStyle={{
+        width: '100%',
+        height: '100%'
+      }}
+      containerClassName="map"
+      />
+        </div>
     );
   }
 }
+
+export default LSMap;
