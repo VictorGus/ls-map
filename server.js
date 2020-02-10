90e4768a8830b34221f90e16d215981f2e02e863
VictorGus <retaow@gmail.com>
2020-02-11 00:19:26 +0300

    Keep trying

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 
-const Universities = YAML.parse(fs.readFileSync('../resources/test.yaml', 'utf-8'));
+const Universities = YAML.parse(fs.readFileSync('./resources/test.yaml', 'utf-8'));
 
 app.use(express.static(path.join(__dirname, '/ui/build')));
 
 app.use(function(req, res, next ){
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
